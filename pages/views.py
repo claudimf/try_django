9bdb7c44fcbeefdf5bed4c53c08ae1b48fcd487e
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-04 18:25:16 -0300

    Creates anothers path to routes

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(request.user)
     return HttpResponse("<h1>Hello World<h1>")
+
+def contact_view(request, *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1>Contact Page<h1>")
