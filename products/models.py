2f193b519236fb77e6c281283f788d2b4ca5be46
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-04 17:24:18 -0300

    Updates Product models like demanded in class 11th

--- products/models.py
+++ products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField()
