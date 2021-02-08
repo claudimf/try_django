c63aabb92aebf20b49bbaa8956eb790b74fe1a55
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-08 17:57:43 -0300

    Makes class '30 - Handle DoesNotExist'

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
@@ -73,7 +74,12 @@ def render_initial_data(request):
 
 
 def dynamic_lookup_view(request, my_id):
-    obj = Product.objects.get(id=my_id)
+    # obj = Product.objects.get(id=my_id) #old way
+    obj = get_object_or_404(Product, id=my_id) #with get_object_or_404
+    # try:
+    #     obj = Product.objects.get(id=my_id)
+    # except Product.DoesNotExist:
+    #     raise Http404
     context = {
         'object': obj
     }
