8f37f23b0310ee9e5badb76b8c029c4a6a27d069
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-09 15:58:50 -0300

    Makes class '38', creates 'article_create' form

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
                     ArticleDetailView,
-                    ArticleListView
+                    ArticleListView,
+                    ArticleCreateView
                     )
 
 app_name = 'articles'
 urlpatterns = [
     path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
-    path('', ArticleListView.as_view(), name='article-list')
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('create/', ArticleCreateView.as_view(), name='article-create')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
-
 from django.views.generic import (
     DetailView,
-    ListView
+    ListView,
+    CreateView
 )
+from .forms import ArticleModelForm
 
 
 class ArticleListView(ListView):
@@ -19,6 +20,14 @@ class ArticleDetailView(DetailView):
     def get_object(self):
         id_ = self.kwargs.get("id")
         return get_object_or_404(Article, id=id_)
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    # '<blog>/<modelname>_list.html'
+    queryset = Article.objects.all()
+    # 'success_url = '/''
 
 
 # # Old way
