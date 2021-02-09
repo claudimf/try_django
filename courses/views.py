3a621388ddf96c14f8c58a63b9629cc779e9062d
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-09 18:02:06 -0300

    Makes class '41 - Raw Detail Class Based View'

--- courses/views.py
+++ courses/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Course
 
 
 # BASE VIEW Class = VIEW
 class CourseView(View):
-    template_name = 'about.html'
+    template_name = "courses/course_detail.html"  # DetailView
+
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        # context = {'object': self.get_object()}
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
+
+
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
 
     def get(self, request, *args, **kwargs):
-        # GET method
-        return render(request, self.template_name, {})
+        context = {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
 
 
 # HTTP METHODS
