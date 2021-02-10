ffa736ecb0ad02397fd6750c3eacd8265071cb54
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-02-10 13:08:20 -0300

    Finishes the course

--- courses/views.py
+++ courses/views.py
@@ -4,16 +4,28 @@ from .models import Course
 from .forms import CourseModelForm
 
 
+class CourseObjectMixin(object):
+    model = Course
+    lookup = 'id'
+
+    def get_object(self):
+        id = self.kwargs.get(self.lookup)
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(self.model, id=id)
+        return obj
+
+
 # BASE VIEW Class = VIEW
-class CourseView(View):
+class CourseView(CourseObjectMixin, View):
     template_name = "courses/course_detail.html"  # DetailView
 
     def get(self, request, id=None, *args, **kwargs):
         # GET method
-        # context = {'object': self.get_object()}
-        context = {}
+        context = {'object': self.get_object()}
+        # context = {}
         if id is not None:
-            obj = get_object_or_404(Course, id=id)
+            # obj = get_object_or_404(Course, id=id)
             context['object'] = obj
         return render(request, self.template_name, context)
 
@@ -49,15 +61,8 @@ class CourseCreateView(View):
         return render(request, self.template_name, context)
 
 
-class CourseUpdateView(View):
+class CourseUpdateView(CourseObjectMixin, View):
     template_name = "courses/course_update.html"  # DetailView
-
-    def get_object(self):
-        id = self.kwargs.get('id')
-        obj = None
-        if id is not None:
-            obj = get_object_or_404(Course, id=id)
-        return obj
 
     def get(self, request, id=None, *args, **kwargs):
         # GET method
@@ -82,15 +87,8 @@ class CourseUpdateView(View):
         return render(request, self.template_name, context)
 
 
-class CourseDeleteView(View):
+class CourseDeleteView(CourseObjectMixin, View):
     template_name = "courses/course_delete.html"  # DetailView
-
-    def get_object(self):
-        id = self.kwargs.get('id')
-        obj = None
-        if id is not None:
-            obj = get_object_or_404(Course, id=id)
-        return obj
 
     def get(self, request, id=None, *args, **kwargs):
         # GET method
